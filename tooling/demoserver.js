ccdb83a96a653a269e05ea4b1cde46ff1c0762ae
Alexander R. Krug <krug@coll.mpg.de>
2021-11-08 15:00:24 +0100

    remove old tooling files

--- tooling/demoserver.js
+++
@@ -1,8 +0,0 @@
-var server=require('node-http-server');
-server.deploy(
-    {
-        port:8000,
-        root: __dirname + '/../demo/'
-    }
-);
-console.log('Demo Server running at http://localhost:8000')
